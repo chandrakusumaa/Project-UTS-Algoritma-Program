336860bc6daaba0c7e09f4fc82d51c83e3daa307
chandrakusumaa <chandrakusumac66@gmail.com>
2026-04-25 20:50:07 +0700

    ini udah sampai play

--- ProjectUTS.cpp
+++ ProjectUTS.cpp
@@ -2,69 +2,140 @@
 #include<string>
 using namespace std;
 
-struct lagu{
+struct node { //membuat struktur node
     string judul;
     string genre;
     string artis;
     double durasi;
+    node* next;
+    node* prev;
 };
 
-void inputlagu(lagu *l, int jumlah){
-    for(int i = 0; i < jumlah; i++){
-        cout << "Data lagu ke-" << i+1 << endl;
-        cin.ignore();
-        cout << "Judul lagu : ";
-        getline(cin, l[i].judul);
-        cout << "Genre lagu : ";
-        getline(cin, l[i].genre);
-        cout << "Artis : ";
-        getline(cin, l[i].artis);
-        cout << "Durasi lagu : ";
-        cin >> l[i].durasi;
-    }
-}
+node* head = NULL;
+node* current = NULL;
 
-inline void tampilkanlagu (lagu *l, int jumlah){ // fungsi inline ini untuk menampilkan seluruh data dari seluruh lagu
-    for (int i = 0; i < jumlah; i++){
-    cout << "\n Data lagu Ke-" << i+1 << endl;
-    cout << "Judul lagu = " << l[i].judul << endl;
-    cout << "Genre lagu = " << l[i].genre << endl;
-    cout << "Artis = " << l[i].artis << endl;
-    cout << "Durasi lagu = " << l[i].durasi << endl;
-    }
-}
-
-void hapuslagu(lagu *l, int &jumlah){ //fungsi ini untuk menghapus lagu
-    string dicari;
-    cout << "Judul lagu = ";
+void inputlagu(int jumlah){
+    
     cin.ignore();
-    getline(cin, dicari);
 
-    int index = -1;
+    for (int i = 0; i < jumlah; i++) {
+       
+        cout << "Data lagu ke-" << i+1 << endl;
+        node* baru = new node; //membuat node baru
 
-    bool ditemukan = false;
+        cout << "Judul lagu : ";// memasukan data lagu
+        getline(cin, baru->judul);
+        cout << "Genre lagu : ";
+        getline(cin, baru->genre);
+        cout << "Artis : ";
+        getline(cin, baru->artis);
+        cout << "Durasi lagu : ";
+        cin >> baru->durasi;
+        cin.ignore();
 
-    for(int i = 0; i < jumlah; i++){
-        if(l[i].judul == dicari){ // bagian ini untuk mencari judul yg ingin di hapus
-            index = i;
-            break;
+       if (head == NULL) { //jika data masih kosong
+            head = baru;
+            baru->next = baru;
+            baru->prev = baru;
+        } else { //jika sudah ada data
+            node* tail = head->prev;
+
+            tail->next = baru;
+            baru->prev = tail;
+            baru->next = head;
+            head->prev = baru;
         }
     }
 
-    //kalo ga nemu
-    if(index == -1) {
-        cout << "lagu tidak ditemukan" << endl;
+    cout << "\nLagu berhasil ditambahkan!\n";
+
+}
+
+void tampilkan() {
+    if (head == NULL) { //apakah ada data lagu atau tidak
+        cout << "Tidak ada lagu\n";
+        return;
+    }
+    node* temp = head; 
+    int no = 1;
+    do{ //pengulangan program untuk menampilkan seluruh data
+        cout << "Lagu Ke-:\n" << no++;
+        cout << "Judul  : " << temp->judul << endl;
+        cout << "Artis  : " << temp->artis << endl;
+        cout << "Genre  : " << temp->genre << endl;
+        cout << "Durasi : " << temp->durasi << endl;
+        temp = temp->next;
+
+    }while(temp != head);
+    
+}
+
+void hapusLagu() {
+    if (head == NULL) {
+        cout << "Tidak ada lagu\n";
         return;
     }
 
-    for(int i = index; i < jumlah - 1; i++){ // menapilkan jika judul tidak ditemukan
-        l[i] = l[i + 1]; 
+    string dicari;
+    cout << "Masukkan judul lagu: ";
+    cin.ignore();
+    getline(cin, dicari);
 
+    node* temp = head;
+    bool ditemukan = false;
+
+    do {
+        if (temp->judul == dicari) {
+            ditemukan = true;
+
+            // jika hanya 1 node
+            if (temp->next == temp) {
+                head = NULL;
+                current = NULL;
+            } 
+            else {
+                //  update head jika yang dihapus head
+                if (temp == head) {
+                    head = head->next;
+                }
+
+                //  update current jika yang dihapus sedang diputar
+                if (temp == current) {
+                    current = temp->next;
+                }
+
+                //  sambung ulang node
+                temp->prev->next = temp->next;
+                temp->next->prev = temp->prev;
+            }
+
+            delete temp;
+            cout << "Lagu berhasil dihapus\n";
+            return;
+        }
+
+        temp = temp->next;
+
+    } while (temp != head);
+
+    // jika tidak ditemukan
+    if (!ditemukan) {
+        cout << "Lagu tidak ditemukan\n";
+    }
+}
+
+void playLagu() {
+    if (head == NULL) { //jika list lagu kosong
+        cout << "Tidak ada lagu!\n";
+        return;
     }
 
-        jumlah--;
-        cout << "Lagu berhasil dihapus" << endl;
-
+    if (current == NULL) { //jika tidak ada lagu yang sedang diputar
+        current = head;
     }
 
+    cout << "Memutar Lagu\n";
+    cout << "Judul  : " << current->judul << endl;
+    cout << "Artis  : " << current->artis << endl;
 
+}
